53ce3836664e4902750c8be7c6898993c312d14b
Weihong Guan <weihong.guan@gmail.com>
2015-12-11 15:00:47 +0800

    import zepto to do ajax

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-(function(){
-  console.log('hello, world.');
+'use strict';
+
+import $ from 'webpack-zepto';
+$.ajaxSettings.crossDomain = true;
+
+(() => {
+  $.getJSON('http://127.0.0.1:8200/categories', function(data) {
+    console.log(data);
+  });
 })();
