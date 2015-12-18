3113b002ffbbdac8780a27e1310408592c6d161d
Weihong Guan <weihong.guan@gmail.com>
2015-12-18 12:07:33 +0800

    nest route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+var Foo = Vue.extend({
+  template: '<router-view></router-view>'
+});
+
 Vue.use(Router);
 
 var router = new Router({
@@ -19,110 +23,125 @@ router.map({
   '/getting_started': {
     component: require('./components/views/getting_started.vue')
   },
-  '/css/color': {
-    component: require('./components/views/css/color.vue')
+  '/css': {
+    component: Foo,
+    subRoutes: {
+      '/color': {
+        component: require('./components/views/css/color.vue')
+      },
+      '/grid': {
+        component: require('./components/views/css/grid.vue')
+      },
+      '/helpers': {
+        component: require('./components/views/css/helpers.vue')
+      },
+      '/css_media': {
+        component: require('./components/views/css/css_media.vue')
+      },
+      '/sass': {
+        component: require('./components/views/css/sass.vue')
+      },
+      '/shadow': {
+        component: require('./components/views/css/shadow.vue')
+      },
+      '/table': {
+        component: require('./components/views/css/table.vue')
+      },
+      '/typography': {
+        component: require('./components/views/css/typography.vue')
+      }
+    }
   },
-  '/css/grid': {
-    component: require('./components/views/css/grid.vue')
+  '/components': {
+    component: Foo,
+    subRoutes: {
+      '/badges': {
+        component: require('./components/views/components/badges.vue')
+      },
+      '/buttons': {
+        component: require('./components/views/components/buttons.vue')
+      },
+      '/breadcrumbs': {
+        component: require('./components/views/components/breadcrumbs.vue')
+      },
+      '/cards': {
+        component: require('./components/views/components/cards.vue')
+      },
+      '/chips': {
+        component: require('./components/views/components/chips.vue')
+      },
+      '/collections': {
+        component: require('./components/views/components/collections.vue')
+      },
+      '/footer': {
+        component: require('./components/views/components/footer.vue')
+      },
+      '/forms': {
+        component: require('./components/views/components/forms.vue')
+      },
+      '/icons': {
+        component: require('./components/views/components/icons.vue')
+      },
+      '/navbar': {
+        component: require('./components/views/components/navbar.vue')
+      },
+      '/pagination': {
+        component: require('./components/views/components/pagination.vue')
+      },
+      '/preloader': {
+        component: require('./components/views/components/preloader.vue')
+      }
+    }
   },
-  '/css/helpers': {
-    component: require('./components/views/css/helpers.vue')
-  },
-  '/css/css_media': {
-    component: require('./components/views/css/css_media.vue')
-  },
-  '/css/sass': {
-    component: require('./components/views/css/sass.vue')
-  },
-  '/css/shadow': {
-    component: require('./components/views/css/shadow.vue')
-  },
-  '/css/table': {
-    component: require('./components/views/css/table.vue')
-  },
-  '/css/typography': {
-    component: require('./components/views/css/typography.vue')
-  },
-  '/components/badges': {
-    component: require('./components/views/components/badges.vue')
-  },
-  '/components/buttons': {
-    component: require('./components/views/components/buttons.vue')
-  },
-  '/components/breadcrumbs': {
-    component: require('./components/views/components/breadcrumbs.vue')
-  },
-  '/components/cards': {
-    component: require('./components/views/components/cards.vue')
-  },
-  '/components/chips': {
-    component: require('./components/views/components/chips.vue')
-  },
-  '/components/collections': {
-    component: require('./components/views/components/collections.vue')
-  },
-  '/components/footer': {
-    component: require('./components/views/components/footer.vue')
-  },
-  '/components/forms': {
-    component: require('./components/views/components/forms.vue')
-  },
-  '/components/icons': {
-    component: require('./components/views/components/icons.vue')
-  },
-  '/components/navbar': {
-    component: require('./components/views/components/navbar.vue')
-  },
-  '/components/pagination': {
-    component: require('./components/views/components/pagination.vue')
-  },
-  '/components/preloader': {
-    component: require('./components/views/components/preloader.vue')
-  },
-  '/javascript/collapsible': {
-    component: require('./components/views/javascript/collapsible.vue')
-  },
-  '/javascript/dialogs': {
-    component: require('./components/views/javascript/dialogs.vue')
-  },
-  '/javascript/dropdown': {
-    component: require('./components/views/javascript/dropdown.vue')
-  },
-  '/javascript/media': {
-    component: require('./components/views/javascript/media.vue')
-  },
-  '/javascript/slider_demo': {
-    component: require('./components/views/javascript/slider_demo.vue')
-  },
-  '/javascript/modals': {
-    component: require('./components/views/javascript/modals.vue')
-  },
-  '/javascript/parallax': {
-    component: require('./components/views/javascript/parallax.vue')
-  },
-  '/javascript/parallax_demo': {
-    component: require('./components/views/javascript/parallax_demo.vue')
-  },
-  '/javascript/pushpin': {
-    component: require('./components/views/javascript/pushpin.vue')
-  },
-  '/javascript/scrollfire': {
-    component: require('./components/views/javascript/scrollfire.vue')
-  },
-  '/javascript/scrollspy': {
-    component: require('./components/views/javascript/scrollspy.vue')
-  },
-  '/javascript/sidenav': {
-    component: require('./components/views/javascript/sidenav.vue')
-  },
-  '/javascript/tabs': {
-    component: require('./components/views/javascript/tabs.vue')
-  },
-  '/javascript/transitions': {
-    component: require('./components/views/javascript/transitions.vue')
-  },
-  '/javascript/waves': {
-    component: require('./components/views/javascript/waves.vue')
+  '/javascript': {
+    component: Foo,
+    subRoutes: {
+      '/collapsible': {
+        component: require('./components/views/javascript/collapsible.vue')
+      },
+      '/dialogs': {
+        component: require('./components/views/javascript/dialogs.vue')
+      },
+      '/dropdown': {
+        component: require('./components/views/javascript/dropdown.vue')
+      },
+      '/media': {
+        component: require('./components/views/javascript/media.vue')
+      },
+      '/slider_demo': {
+        component: require('./components/views/javascript/slider_demo.vue')
+      },
+      '/modals': {
+        component: require('./components/views/javascript/modals.vue')
+      },
+      '/parallax': {
+        component: require('./components/views/javascript/parallax.vue')
+      },
+      '/parallax_demo': {
+        component: require('./components/views/javascript/parallax_demo.vue')
+      },
+      '/pushpin': {
+        component: require('./components/views/javascript/pushpin.vue')
+      },
+      '/scrollfire': {
+        component: require('./components/views/javascript/scrollfire.vue')
+      },
+      '/scrollspy': {
+        component: require('./components/views/javascript/scrollspy.vue')
+      },
+      '/sidenav': {
+        component: require('./components/views/javascript/sidenav.vue')
+      },
+      '/tabs': {
+        component: require('./components/views/javascript/tabs.vue')
+      },
+      '/transitions': {
+        component: require('./components/views/javascript/transitions.vue')
+      },
+      '/waves': {
+        component: require('./components/views/javascript/waves.vue')
+      }
+    }
   },
 
   '/mobile': {
