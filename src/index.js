3a53cb5fb81b21e9de48f81e10abee13df50cb1f
Weihong Guan <weihong.guan@gmail.com>
2016-01-07 09:56:20 +0800

    merge app.vue to index.js

--- src/index.js
+++ src/index.js
@@ -178,7 +178,54 @@ router.beforeEach(function () {
   window.scrollTo(0, 0)
 });
 
+
+var $ = window.jQuery = require('jquery');
+// require('materialize-css/bin/materialize.js');
+
+require('jquery-easing');
+
+require("materialize-css/js/animation.js");
+require("velocity-animate");
+
+require("materialize-css/js/jquery.hammer.js");
+require("imports?$=jquery!materialize-css/js/global.js");
+// require("materialize-css/js/collapsible.js");
+require("./javascripts/collapsible.js");
+// require("materialize-css/js/dropdown.js");
+require("./javascripts/dropdown.js");
+require("materialize-css/js/leanModal.js");
+// require("materialize-css/js/materialbox.js");
+require("./javascripts/materialbox.js");
+require("materialize-css/js/parallax.js");
+require("materialize-css/js/tabs.js");
+require("./javascripts/tabs.js");
+// require("materialize-css/js/tooltip.js");
+require("./javascripts/tooltip.js");
+require("materialize-css/js/waves.js");
+// require("materialize-css/js/toasts.js");
+
+require("materialize-css/js/sideNav.js");
+require("materialize-css/js/scrollspy.js");
+require("materialize-css/js/forms.js");
+require("materialize-css/js/slider.js");
+require("materialize-css/js/cards.js");
+require("materialize-css/js/chips.js");
+require("./javascripts/pushpin.js");
+require("materialize-css/js/buttons.js");
+// require("materialize-css/js/transitions.js");
+// require("materialize-css/js/scrollFire.js");
+
+require('imports?define=>false!pickadate/lib/picker.js');
+require('imports?define=>false!pickadate/lib/picker.date.js');
+
+// require("materialize-css/js/date_picker/picker.js");
+// require("materialize-css/js/date_picker/picker.date.js");
+
+require("materialize-css/js/character_counter.js");
+
+require('prismjs/themes/prism.css');
+
 require('./assets/style.scss');
 
-import App from './components/app.vue'
-router.start(App, 'template');
+// import App from './components/app.vue'
+router.start(FullScreen, 'template');
